7c93ca0aef23ccd90e1e71269d268eede45771ed
Or-Arbel <orarbel1995@gmail.com>
2022-05-19 16:10:12 +0300

    fix date format and validations on admin panel

--- src/components/Assets/MaterialTable/hebrewColumns.js
+++ src/components/Assets/MaterialTable/hebrewColumns.js
@@ -6,34 +6,101 @@ const DepartmentsList = [
   { value: "2", label: "תקשוב" },
   { value: "3", label: "חמל אגם" },
   { value: "4", label: "רפואה" },
-  { value: "4", label: "אוכלוסייה וחומס" },
-  { value: "4", label: "מכס כשירות כוחות" },
+  { value: "5", label: "אוכלוסייה וחומס" },
+  { value: "6", label: "מכס כשירות כוחות" },
 ];
 
+const dateFormat = "dd/MM/yyyy, hh:mm:ss";
+
 const hebrewColumns = (tableName) => {
-  let arr;
   switch (tableName) {
     case "links":
       return [
         { title: "סדר", field: "order", defaultSort: "asc", editable: "never" },
-        { title: "שם", field: "name" },
-        { title: "לינק", field: "url" },
-        { title: "נוצר בתאריך", field: "createdAt", editable: "never" },
-        { title: "עודכן בתאריך", field: "updatedAt", editable: "never" },
+        {
+          title: "שם",
+          field: "name",
+          validate: (rowData) =>
+            rowData.name.length < 2 || rowData.name.length > 40
+              ? "נא הזן שם באורך 2-40 תווים"
+              : "",
+        },
+        {
+          title: "לינק",
+          field: "url",
+          validate: (rowData) =>
+            rowData.url.length < 2 || rowData.url.length > 40
+              ? "נא הזן url באורך 2-40 תווים"
+              : "",
+        },
+        {
+          title: "נוצר בתאריך",
+          field: "createdAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
+        {
+          title: "עודכן בתאריך",
+          field: "updatedAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
       ];
     case "departments":
       return [
         { title: "סדר", field: "order", defaultSort: "asc", editable: "never" },
-        { title: "שם", field: "name" },
-        { title: "נוצר בתאריך", field: "createdAt", editable: "never" },
-        { title: "עודכן בתאריך", field: "updatedAt", editable: "never" },
+        {
+          title: "שם",
+          field: "name",
+          validate: (rowData) =>
+            rowData.name.length < 3 || rowData.name.length > 40
+              ? "נא הזן שם באורך 3-40 תווים"
+              : "",
+        },
+        {
+          title: "נוצר בתאריך",
+          field: "createdAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
+        {
+          title: "עודכן בתאריך",
+          field: "updatedAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
       ];
     case "shobDevelopments":
       return [
         { title: "סדר", field: "order", defaultSort: "asc", editable: "never" },
-        { title: "כותרת", field: "title" },
-        { title: "כותרת משנה", field: "subTitle" },
-        { title: "תיאור", field: "description" },
+        {
+          title: "כותרת",
+          field: "title",
+          validate: (rowData) =>
+            rowData.title.length < 3 || rowData.title.length > 40
+              ? "נא הזן כותרת באורך 3-40 תווים"
+              : "",
+        },
+        {
+          title: "כותרת משנה",
+          field: "subTitle",
+          validate: (rowData) =>
+            rowData.subTitle.length < 3 || rowData.subTitle.length > 150
+              ? "נא הזן כותרת משנה באורך 3-150 תווים"
+              : "",
+        },
+        {
+          title: "תיאור",
+          field: "description",
+          validate: (rowData) =>
+            rowData.description.length < 3 || rowData.description.length > 1500
+              ? "נא הזן תיאור באורך 3-1500 תווים"
+              : "",
+        },
         {
           title: "תמונה",
           field: "image",
@@ -41,14 +108,34 @@ const hebrewColumns = (tableName) => {
             <img src={rowData.image} width="50" height="60" />
           ),
         },
-        { title: "נוצר בתאריך", field: "createdAt", editable: "never" },
-        { title: "עודכן בתאריך", field: "updatedAt", editable: "never" },
+        {
+          title: "נוצר בתאריך",
+          field: "createdAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
+        {
+          title: "עודכן בתאריך",
+          field: "updatedAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
       ];
     case "departmentLinks":
       return [
         { title: "סדר", field: "order", defaultSort: "asc", editable: "never" },
-        { title: "שם", field: "name" },
-        { title: "כתובת url", field: "url" },
+        {
+          title: "שם",
+          field: "name",
+          validate: (rowData) => (rowData.name === "" ? "נא הזן שם" : ""),
+        },
+        {
+          title: "כתובת url",
+          field: "url",
+          validate: (rowData) => (rowData.url === "" ? "נא הזן url" : ""),
+        },
         {
           title: "מחלקה",
           field: "departmentId",
@@ -58,15 +145,31 @@ const hebrewColumns = (tableName) => {
           editComponent: ({ value, onChange }) => (
             <Select
               options={DepartmentsList}
-              name="fruitSelect"
+              name="departmentsSelect"
               onChange={(selectedOption) => onChange(selectedOption.value)}
               value={value ? value.value : value}
               placeholder="בחר מחלקה"
             />
           ),
+          validate: (rowData) =>
+            rowData.departmentId === null || rowData.departmentId === undefined
+              ? "נא בחר מחלקה, לינק צריך להיות משוייך למחלקה"
+              : "",
         },
-        { title: "נוצר בתאריך", field: "createdAt", editable: "never" },
-        { title: "עודכן בתאריך", field: "updatedAt", editable: "never" },
+        {
+          title: "נוצר בתאריך",
+          field: "createdAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
+        {
+          title: "עודכן בתאריך",
+          field: "updatedAt",
+          editable: "never",
+          type: "datetime",
+          dateSetting: { format: dateFormat },
+        },
       ];
 
     //   default:
